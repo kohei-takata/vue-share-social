f73b706a48a64273619f061d5baeef037d8815fa
kubotak <kubotak@istyle.co.jp>
2018-07-22 00:53:42 +0900

    add: svg

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <ShareFacebook
-     url="http://recruit.istyle.co.jp/career/"
+      url="http://recruit.istyle.co.jp/career/"
     />
     <ShareTwitter
-            url="http://recruit.istyle.co.jp/career/"
-            title="recruit page"
+      url="http://recruit.istyle.co.jp/career/"
+      title="recruit page"
     />
+
+    <div>
+      <FacebookLogo/>
+      <TwitterLogo/>
+      <HatenaLogo/>
+      <GooglePlusLogo/>
+      <LineLogo/>
+    </div>
   </div>
 </template>
 
@@ -14,11 +22,22 @@
 import ShareFacebook from '@/components/ShareFacebook.vue'
 import ShareTwitter from '@/components/ShareTwitter.vue'
 
+import FacebookLogo from '@/components/svg/FacebookLogo.vue'
+import TwitterLogo from '@/components/svg/TwitterLogo.vue'
+import GooglePlusLogo from '@/components/svg/GooglePlusLogo.vue'
+import HatenaLogo from '@/components/svg/HatenaLogo.vue'
+import LineLogo from '@/components/svg/LineLogo.vue'
+
 export default {
   name: 'app',
   components: {
     ShareFacebook,
-    ShareTwitter
+    ShareTwitter,
+    FacebookLogo,
+    TwitterLogo,
+    GooglePlusLogo,
+    HatenaLogo,
+    LineLogo
   }
 }
 </script>
